200522ec367a106c39b7c5562a42b46a33fbe2de
Toft <toft.dah@gmail.com>
2025-06-30 09:41:24 +0300

    local play

--- server.mjs
+++ server.mjs
@@ -66,3 +66,7 @@ export function stopMiniGame() {
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000')
 })
+
+// server.listen(3000, '0.0.0.0', () => {
+//     console.log('Server running at http://0.0.0.0:3000')
+// })
